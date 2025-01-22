98690a61c4bc324cbaf4c42e67cc62a39d26e13f
NIKHIL1453-GIT <160570960+NIKHIL1453-GIT@users.noreply.github.com>
2025-01-22 11:23:15 +0530

    assessment Updated

--- Assessment-2/Assess(2)-que1.c
+++
@@ -1,4 +0,0 @@
-/*Create a Project to demonstrate Simple Food Ordering Project which will help to order 
-foods . The Project should contain all the necessary  requirements i.e  need to use 
-conditional Statements, Loops, Function, SwitchCase  and also A Program should use  
-the concept of OOP.*/
